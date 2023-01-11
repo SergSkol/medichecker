5b65383a86f13d395bc19744ef6ac79c2694024a
Serg Skol <sps676@gmail.com>
2023-01-11 14:51:03 +0100

    Add info

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
+import Info from './components/Info';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:name" element={<Details />} />
+      <Route path="/info/" element={<Info />} />
       <Route path="*" element={<p>Path is not resolved</p>} />
     </Routes>
   </BrowserRouter>
